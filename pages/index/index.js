04c20e836bfd91211da0632b96a45db4e219d3c3
caracacara22 <13051725950@163.com>
2024-08-28 09:47:48 +0800

    why I change it

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,7 +1,7 @@
 // index.js
 const defaultAvatarUrl = 'https://mmbiz.qpic.cn/mmbiz/icTdbqWNOwNRna42FI242Lcia07jQodd2FJGIYQfG0LAJGFxM4FbnQP6yfMxBgJ0F3YRqJCJ1aPAK2dQagdusBZg/0'
 
-Component({
+Page({
   data: {
 
     },
@@ -10,13 +10,13 @@ Component({
     },
   
   getUserProfile(e) {
+    var that = this;
     wx.getUserProfile({
       desc: '展示用户信息', // 声明获取用户个人信息后的用途，后续会展示在弹窗中，请谨慎填写
       success: (res) => {
         console.log(res)
-        this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
+        that.setData({
+          userInfo: res.userInfo
          })
       }
     })
