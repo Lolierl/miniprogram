dd0faf7aaada9c6d1ddd8c6f023daef30db87006
刘思远 <liusiyua23@mails.tsinghua.edu.cn>
2024-09-03 20:10:39 +0800

    add picture

--- pages/message/message.js
+++ pages/message/message.js
@@ -24,14 +24,9 @@ Page({
       }
     });
     this.loadChatMessages(currentUserNum, otherUserNum);
+
   },
 
-  
-
-
-
-
-  // 获取当前用户的 num，假设从本地存储获取
   getCurrentUserNum: function() {
     return wx.getStorageSync('userInfo').num;
   },
@@ -44,7 +39,10 @@ Page({
         { senderNum: otherUserNum, receiverNum: currentUserNum }
       ])
     ).orderBy('timestamp', 'asc').get()
-      .then(res => this.setData({ chatMessages: res.data }))
+      .then(res => {
+        console.log(res); // Log the entire response object
+        this.setData({ chatMessages: res.data });
+      })
       .catch(console.error);
   },
   onInput: function(e) {
@@ -62,6 +60,7 @@ Page({
         senderNum: currentUserNum,
         receiverNum: otherUserNum,
         message: messageText,
+        messageType: 'text', 
         timestamp: Date.now()
       }
     }).then(() => {
@@ -73,9 +72,125 @@ Page({
       this.onLoad(options); 
     }).catch(console.error);
   },
+  chooseImage: function() {
+    wx.chooseImage({
+      count: 1,
+      sourceType: ['album', 'camera'],
+      success: (res) => {
+        const filePath = res.tempFilePaths[0];
+        const cloudPath = `images/${Date.now()}-${Math.floor(Math.random(0, 1) * 1000)}.png`;
+
+        wx.cloud.uploadFile({
+          cloudPath,
+          filePath,
+          success: uploadRes => {
+            this.sendImageMessage(uploadRes.fileID);
+          },
+          fail: e => {
+            wx.showToast({
+              icon: 'none',
+              title: 'Upload failed',
+            });
+            console.error('[上传文件] 失败：', e);
+          }
+        });
+      }
+    });
+  },
+
+  // 发送图片消息
+  sendImageMessage: function(imageFileID) {
+    const { currentUserNum, otherUserNum } = this.data;
+    const db = wx.cloud.database();
+
+    db.collection('messages').add({
+      data: {
+        senderNum: currentUserNum,
+        receiverNum: otherUserNum,
+        message: imageFileID, // 这里存储的是图片的 fileID
+        messageType: 'image', // 标识为图片消息
+        imageStatus: 'new image', 
+        timestamp: Date.now()
+      }
+    }).then(() => {
+      this.updateChat('[New Image]');
+      const options = {
+        otherUserNum: this.data.otherUserNum
+      };
+      this.onLoad(options); 
+    }).catch(console.error);
+  },
+  previewImage: function(e) {
+    const currentImageUrl = e.currentTarget.dataset.src;
+    console.log(currentImageUrl)
+    wx.previewImage({
+      current: currentImageUrl, // 当前显示图片的http链接
+      urls: [currentImageUrl] // 需要预览的图片http链接列表
+    });
+  },
+
+  // 处理"exchange"按钮点击
+  exchangeImage: function(e) {
+    const messageId = e.currentTarget.dataset.messageid;
+    const db = wx.cloud.database();
+
+    // 选择图片进行回复
+    wx.chooseImage({
+      count: 1,
+      sourceType: ['album', 'camera'],
+      success: (res) => {
+        const filePath = res.tempFilePaths[0];
+        const cloudPath = `images/${Date.now()}-${Math.floor(Math.random(0, 1) * 1000)}.png`;
+
+        wx.cloud.uploadFile({
+          cloudPath,
+          filePath,
+          success: uploadRes => {
+            this.replyWithImage(messageId, uploadRes.fileID);
+          },
+          fail: e => {
+            wx.showToast({
+              icon: 'none',
+              title: 'Upload failed',
+            });
+            console.error('[上传文件] 失败：', e);
+          }
+        });
+      }
+    });
+  },
+  replyWithImage: function(messageId, imageFileID) {
+    const { currentUserNum, otherUserNum } = this.data;
+    const db = wx.cloud.database();
+    // 发送回复的图片
+    db.collection('messages').add({
+      data: {
+        senderNum: currentUserNum,
+        receiverNum: otherUserNum,
+        message: imageFileID,
+        messageType: 'image',
+        imageStatus: 'new image', 
+        timestamp: Date.now()
+      }
+    }).then(() => {
+      this.updateChat('[New Image]');
+      // 更新原先图片的状态为"replied message"
+      return db.collection('messages').doc(messageId).update({
+        data: {
+          imageStatus: 'replied message'
+        }
+      });
+    }).then(() => {
+      const options = {
+        otherUserNum: this.data.otherUserNum
+      };
+      this.onLoad(options); 
+    }).catch(console.error);
+  },
   navigateBack: function() {
     wx.navigateBack();
   },
+
   updateChat: function(lastMessage) {
     const db = wx.cloud.database();
     const { currentUserNum, otherUserNum } = this.data;
@@ -86,6 +201,13 @@ Page({
         { Auser: otherUserNum, Buser: currentUserNum }
       ]
     }).get().then(res => {
+      db.collection('notifications').add({
+        data: {
+          userNum: otherUserNum,
+          chatUserNum: currentUserNum, 
+          messageId: res._id,
+          isRead: false
+        }})
         return db.collection('chat_users').doc(res.data[0]._id).update({
           data: {
             lastMessage: lastMessage,
