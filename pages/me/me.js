1f3718e37d3f53577059d6ba1229386aa5d210ed
caracacara22 <13051725950@163.com>
2024-09-12 20:28:35 +0800

    why I change it

--- pages/me/me.js
+++ pages/me/me.js
@@ -29,7 +29,48 @@ Page({
         }
       }
     })
+  },
+  ChangeAvatar(event) {
+    const { avatarUrl } = event.detail;
+    const db = wx.cloud.database();
+    wx.showLoading({
+      title: 'Uploading...',
+    });
+    console.log('a');
 
-    
+    wx.cloud.uploadFile({
+      cloudPath: "avatarImages/" + Math.random() + Date.now() + '.png',
+      
+      filePath: event.detail.avatarUrl
+    })
+    .then(res => {
+      const fileID = res.fileID;
+      console.log('b');
+      // 假设你有用户的唯一标识符（userID）
+      const num = this.data.userInfo.num; // 从存储中获取用户ID或其他方式
+      console.log(num);
+      console.log('c');
+      // 更新数据库中的用户数据
+      db.collection('login_users').where({num:num}).update({
+        data: {
+          avatarUrl: fileID
+        }
+      });
+    })
+    .then(() => {
+      wx.hideLoading();
+      wx.showToast({
+        title: 'Avatar updated',
+      });
+    })
+    .catch(error => {
+      console.error('Update failed', error);
+      wx.hideLoading();
+      wx.showToast({
+        title: 'Update failed',
+        icon: 'none'
+      });
+    });
   }
+  
 })
